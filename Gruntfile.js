014893a7468ad8205b26e238ffd2d73ee0e9d6dd
ANGIE D <a.delllano@miami.edu>
2015-11-02 19:37:50 -0500

    first commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,7 @@ module.exports = function (grunt) {
                 options: {
                     watchTask: true,
                     // fill in proxy address of local WP server
-                    proxy: ""
+                    proxy: "http://angiedelllano.local:8888/"
                 }
             }
         }
